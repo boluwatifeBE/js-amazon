c7d72a5a999fca8aeb3ade04033873b31e8b8f42
boluwatifeBE <boluwatifeemmanuel.be@gmail.com>
2025-10-09 14:32:34 +0100

    Move addToCart to cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
 export const cart = [];
+
+export function addToCart(productId) {
+  let existingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      existingItem = cartItem;
+    }
+  });
+
+  if (existingItem) {
+    existingItem.quantity++;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: 1,
+    });
+  }
+}
